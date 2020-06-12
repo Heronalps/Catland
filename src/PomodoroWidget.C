7973fe34105515c220de2d642373062e3a378cd4
Michael Zhang <heronalps@gmail.com>
2020-06-12 03:31:51 -0700

    mz - Update commentMap

--- src/PomodoroWidget.C
+++ src/PomodoroWidget.C
@@ -26,10 +26,15 @@ namespace {
   "Occulus", "Play instrument", "Barbeque", "Run a mile", "Snacks", "Tik Tok", "Find UFO", "TV", "Walk", 
   "Crossfit", "Dash a yard", "Zoom Call a friend"});
   
-  static std::vector<std::string> commentMap({"Aerobics", "Benching", "Cartoon", "Dance", "Eat", "Family Game", 
-  "Video Game", "Hackers New", "Ice Breaker", "Juggling", "Karaoke", "Laugh", "Music", "Netflix", 
-  "Occulus", "Play instrument", "Barbeque", "Run a mile", "Snacks", "Tik Tok", "Find UFO", "TV", "Walk", 
-  "Crossfit", "Dash a yard", "Zoom Call a friend"});
+  static std::vector<std::string> commentMap({"Burn those calories!", "Become a monster!", 
+  "Invoke your inner kid", "You don't need wings to fly!", "What else could be better?", 
+  "Family always comes first!", "Your best friend in quarantine.", "YCombinator rocks!", 
+  "Don't be shy to make new friends!", "We all juggle in our daily lives!", "Sing your soul out!", 
+  "You don't care laugh line!", "Country or Rap?", "Time to binge-watching!", "Immerse into VR.", 
+  "Guitar or piano makes you chill!", "Get yourself somking hot!", "Can you finish in 8 mins?", 
+  "Watch your waistline, please!", "Who needs Instagram?", "The truth is out there!", 
+  "Being old-school is cool!", "Enjoy gorgeous sunset!", "Things don't kill you make you stronger!", 
+  "That feels like \"the longest yard!\"", "Stay connected and engaged!"});
 
   std::vector<int> selectedItem{};
 }
